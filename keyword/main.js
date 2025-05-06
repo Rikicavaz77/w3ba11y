0136c95f4754959ebdc59135b172583518b06ddd
rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2025-05-06 09:50:00 +0200

    add word counter

--- keyword/main.js
+++ keyword/main.js
@@ -2,10 +2,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   let iframe;
   let controller;
 
-  if (message.action === 'runComponents' || message.action === 'updateComponents') {
+  if (message.action === 'runComponents') {
     iframe = document.querySelector('main').shadowRoot.querySelector('iframe').contentDocument;
     controller = new KeywordController(iframe);
     window.keywordController = controller;
-    chrome.runtime.sendMessage({ action: 'finishedComponents', component: 'keyword'});
+    chrome.runtime.sendMessage({ action: 'finishedComponents', component: 'keyword' });
+  }
+
+  if (message.action === 'updateComponents') {
+    iframe = document.querySelector('main').shadowRoot.querySelector('iframe').contentDocument;
+    controller = window.keywordController;
+    if (controller)
+      controller.update(iframe);
   }
 });
