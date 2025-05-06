8d7cf0d43e7bc7b0d77633c85cdde4d23fa9379f
rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
2025-05-06 08:24:13 +0200

    Add keywords analysis overview (#2)
    * add keywords overview
    
    * add event delegation

--- keyword/controller/controller.js
+++ keyword/controller/controller.js
@@ -6,6 +6,24 @@ class KeywordController {
   }
 
   init() {
-    
+    const metaTagKeywordsContent = this.getMetaTagKeywordsContent(this.view.iframe);
+    const lang = this.getLang(this.view.iframe);
+    const overviewInfo = {
+      wordCount: 2300,
+      uniqueWordCount: 1000,
+      metaTagKeywordsContent: metaTagKeywordsContent,
+      lang: lang
+    };
+    this.view.render(overviewInfo);
+  }
+
+  getMetaTagKeywordsContent(doc) {
+    const metaTagKeywordsContent = doc.querySelector("meta[name='keywords' i]")?.content;
+    return metaTagKeywordsContent ?? "Missing";
+  }
+
+  getLang(doc) {
+    const lang = doc.documentElement.lang;
+    return lang || 'Missing';
   }
 }
