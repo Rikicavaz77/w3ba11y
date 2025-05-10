8a3436c1e2efd83ca2cc6c4d824d87799149465d
rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
2025-05-10 09:30:53 +0200

    completed keyword class (#17)

--- keyword/model/keyword.js
+++ keyword/model/keyword.js
@@ -1,13 +1,17 @@
 class Keyword {
-  constructor(name, status = "analyzing") {
+  constructor(name, { status = "analyzing", frequency = 0, density = 0, tagData = null, relevanceScore = 0 } = {}) {
     this._name = name;
     this._status = status;
-    this._frequency = 0;
-    this._density = 0;
-    this._tagData = {
+    this._frequency = frequency;
+    this._density = density;
+    this._tagData = tagData || this.defaultTagData();
+    this._relevanceScore = relevanceScore;
+  }
+
+  defaultTagData() {
+    return {
       title:      { keywordOccurrences: 0, weight: 10, tagOccurrences: 0 },
       description:{ keywordOccurrences: 0, weight: 6, tagOccurrences: 0 },
-      keywords:   { keywordOccurrences: 0, weight: 0, tagOccurrences: 0 },
       h1:         { keywordOccurrences: 0, weight: 5, tagOccurrences: 0 },
       h2:         { keywordOccurrences: 0, weight: 4, tagOccurrences: 0 },
       h3:         { keywordOccurrences: 0, weight: 3, tagOccurrences: 0 },
@@ -17,15 +21,58 @@ class Keyword {
       p:          { keywordOccurrences: 0, weight: 0, tagOccurrences: 0 },
       a:          { keywordOccurrences: 0, weight: 1.5, tagOccurrences: 0 },
       alt:        { keywordOccurrences: 0, weight: 2.5, tagOccurrences: 0 }
-    };
-    this.__relevanceScore = 0;
+    }
   }
 
   get name() {
     return this._name;
   }
 
+  get status() {
+    return this._status;
+  }
+
   get frequency() {
     return this._frequency;
+  }
+
+  get density() {
+    return this._density;
+  }
+
+  get tagData() {
+    return this._tagData;
+  }
+
+  get relevanceScore() {
+    return this._relevanceScore;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set status(status) {
+    this._status = status;
+  }
+
+  set density(density) {
+    this._density = density;
+  }
+
+  set tagData(tagData) {
+    this._tagData = tagData;
+  }
+
+  calculateRelevanceScore() {
+    let score = 0;
+    let maxScore = 0;
+    Object.entries(this.tagData).forEach(([_, data]) => {
+      if (data.weight && data.tagOccurrences > 0) {
+        score += (data.keywordOccurrences / data.tagOccurrences) * data.weight;
+        maxScore += data.weight;
+      }
+    });
+    this.relevanceScore = Math.ceil((score / maxScore) * 100);
   }
 }
