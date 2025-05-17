44005a41cd30c5800193a9bdd50e95cc07aef435
rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
2025-05-17 12:23:44 +0200

    Pre-release 0.0.2 (#27)
    * Most frequent keywords (#23)

--- keyword/services/word_counter.js
+++ keyword/services/word_counter.js
@@ -2,8 +2,14 @@ class WordCounter {
   constructor(textProcessor, tagAccessor) {
     this._textProcessor = textProcessor;
     this._tagAccessor = tagAccessor;
+    this._cachedWords = null;
     this._totalWords = 0;
     this._uniqueWords = 0;
+    this._stopwords = {
+      en: new Set(sw.eng),
+      it: new Set(sw.ita),
+    };
+    this.countWords();
   }
 
   get totalWords() {
@@ -29,7 +35,9 @@ class WordCounter {
     });
   }
 
-  countWords() {
+  _collectWords(forceRefresh = false) {
+    if (!forceRefresh && this._cachedWords) return this._cachedWords;
+
     const pattern = this._textProcessor.getWordsPattern();
     const words = [];
     this.treeWalker.resetWalker();
@@ -38,10 +46,29 @@ class WordCounter {
       const matches = node.nodeValue.toLowerCase().match(pattern) || [];
       words.push(...matches);
     }
+
     ["title", "description", "alt"].forEach(tagName => {
       this._countWordsInTag(tagName, pattern, words);
     });
 
+    this._cachedWords = words;
+    return words;
+  }
+
+  _countOccurrences(words) {
+    const map = new Map();
+    words.forEach(word => {
+      map.set(word, (map.get(word) || 0) + 1);
+    });
+    return map;
+  }
+
+  resetCache() {
+    this._cachedWords = null;
+  }
+
+  countWords(forceRefresh = false) {
+    const words = this._collectWords(forceRefresh);
     this._totalWords = words.length;
     this._uniqueWords = new Set(words).size;
 
@@ -49,5 +76,22 @@ class WordCounter {
       totalWords: this._totalWords,
       uniqueWords: this._uniqueWords
     };
+  }
+
+  findOneWordKeywords(lang = 'en') {
+    const baseLang = lang.split('-')[0].toLowerCase();
+    let stopwords = this._stopwords[baseLang] || new Set();
+    if (baseLang === 'it') {
+      stopwords = new Set([...stopwords, ...this._stopwords['en']]);
+    }
+
+    const words = this._collectWords();
+    const filteredWords = words.filter(word => !stopwords.has(word));
+    const wordsMap = this._countOccurrences(filteredWords);
+    const relevantWords = [...wordsMap.entries()]
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 10)
+      .map(([key, _]) => key);
+    return relevantWords;
   }
 }
