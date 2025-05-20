d37a275e74e081cd3f53d690d6f328e80cfc5d37
rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2025-05-20 17:19:28 +0200

    better escaping

--- keyword/view/view.js
+++ keyword/view/view.js
@@ -232,7 +232,7 @@ class KeywordView {
               </div>
             </div>
             <div class="keywords__overview-item-body">
-              <p class="keywords__overview-item-body__text">${Utils.escapeHTML(overviewInfo.lang) || 'Missing'}</p>
+              <p class="keywords__overview-item-body__text">${overviewInfo.lang || 'Missing'}</p>
             </div>
           </div>          
         </li>
