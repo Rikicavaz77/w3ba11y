93d8cccfa3364c069f3afb4869b8be3888b962b4
rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2025-05-22 08:52:19 +0200

    test strategy modules

--- keyword/services/strategy/keyword_analysis_strategy.js
+++ keyword/services/strategy/keyword_analysis_strategy.js
@@ -1,16 +1,16 @@
 class KeywordAnalysisStrategy {
   constructor() {
     if (new.target === KeywordAnalysisStrategy) {
-      throw new Error("KeywordAnalysisStrategy cannot be instantiated directly");
+      throw new Error('KeywordAnalysisStrategy cannot be instantiated directly');
     }
   }
 
   setContext(context) {
-    throw new Error("setContext() must be implemented");
+    throw new Error('setContext() must be implemented');
   }
 
   analyze(textNodes, pattern, keyword) {
-    throw new Error("analyze() must be implemented");
+    throw new Error('analyze() must be implemented');
   }
 }
 
