00651c9c4c4a7fc84dbe84311885d9d23e70ac9b
rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2025-05-22 08:31:32 +0200

    test allInOne strategy

--- keyword/services/keyword_analyzer.js
+++ keyword/services/keyword_analyzer.js
@@ -90,3 +90,9 @@ class KeywordAnalyzer {
     }
   }
 }
+
+// Export for use in Node environment (testing with Jest). Ignored in browsers
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = KeywordAnalyzer;
+}
+
--- keyword/services/strategy/keyword_analysis_strategy.js
+++ keyword/services/strategy/keyword_analysis_strategy.js
@@ -13,3 +13,8 @@ class KeywordAnalysisStrategy {
     throw new Error("analyze() must be implemented");
   }
 }
+
+// Export for use in Node environment (testing with Jest). Ignored in browsers
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = KeywordAnalysisStrategy;
+}
